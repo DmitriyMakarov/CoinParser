cdbfb9f66be4ea7d98148f7b6a6d537d8846bcc1
systemUser <i@dmitrijmakarov.ru>
2022-12-27 17:35:19 +0300

    add bs4, test parser

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import pandas as pd
 
+
+url = 'https://cbr.ru/cash_circulation/memorable_coins/coins_base/#2'
 
 
 if __name__ == '__main__':
-
-    pass
+    r = requests.get(url)
+    print(r.status_code)
+    soup = bs(r.text, "html.parser")
+    catalog_numbers = soup.find_all('td', class_='coins-list_item_number middle right')
+    names = soup.find_all('td', class_='coins-list_item_name middle')
+    metalls = soup.find_all('td', class_='coins-list_item_metall middle')
+    values = soup.find_all('td', class_='coins-list_item_face-value middle')
+    dates = soup.find_all('td', class_='coins-list_item_date middle right')
+    for number in catalog_numbers:
+        print(number.text)
+    for name in names:
+        print(name.text)
+    for metall in metalls:
+        print(metall.text)
